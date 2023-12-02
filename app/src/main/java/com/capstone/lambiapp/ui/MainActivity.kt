8fc14735f897ba37e28f3347aca03358d1fc4c1e
herjatiaji <herjati.aji@mail.ugm.ac.id>
2023-12-02 19:06:37 +0700

    login bang

--- app/src/main/java/com/capstone/lambiapp/ui/MainActivity.kt
+++ app/src/main/java/com/capstone/lambiapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.capstone.lambiapp
+package com.capstone.lambiapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.capstone.lambiapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
