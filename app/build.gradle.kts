96dc6119bc1832d10e18cffe167bd5dfc258b3b7
herjatiaji <herjati.aji@mail.ugm.ac.id>
2023-12-02 22:38:03 +0700

    login bang

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.capstone.lambiapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.capstone.lambiapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -43,7 +43,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.3.1")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -51,4 +51,5 @@ dependencies {
     implementation("androidx.core:core-splashscreen:1.0.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
+    implementation("org.michaelevans.colorart:library:0.0.3")
 }
--- app/src/main/java/com/capstone/lambiapp/ui/MainActivity.kt
+++ app/src/main/java/com/capstone/lambiapp/ui/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.capstone.lambiapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.service.autofill.TextValueSanitizer
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.capstone.lambiapp.R
+import org.michaelevans.colorart.library.ColorArt
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tv: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv = findViewById(R.id.tvmain)
+        var bitmap = BitmapFactory.decodeResource(resources,R.drawable.loginlogo)
+        var colorArt = ColorArt(bitmap)
+        
+        tv.text = colorArt.primaryColor.toString()
+        
     }
 }
